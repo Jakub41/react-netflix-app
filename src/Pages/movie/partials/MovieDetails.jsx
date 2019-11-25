624420d9e2891f6570fe5fcb0fe6c7548064afe2
jakub41 <lemiszewski@gmx.com>
2019-11-25 15:31:40 +0100

    Rendering the movie info

--- src/Pages/movie/partials/MovieDetails.jsx
+++ src/Pages/movie/partials/MovieDetails.jsx
@@ -23,31 +23,36 @@ export default class MovieDetails extends Component {
     await this.onFetchInfo(this.props.movieID);
   }
 
-  onFetchInfo = async (movieID) => {
+  onFetchInfo = async movieID => {
     try {
       const info = await getMoviesInfo(movieID);
 
       console.log("GETTING IN DETAIL", info);
 
       this.setState({
-        MovieDetails: info.data,
+        movieInfo: info.data,
       });
+
+      return [];
     } catch (err) {
       console.log("onFetchInfo err: ", err);
     }
   };
 
   render() {
-    return <div></div>;
+    const {info} = this.props;
+    return info ? <div><h1>{info.Actors}</h1></div> : <div></div>;
   }
 }
 
 MovieDetails.propTypes = {
   history: PropTypes.any,
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      imdbID: PropTypes.string.isRequired,
-    }),
+  info: PropTypes.shape({
+    Actors: PropTypes.string,
+    Awards: PropTypes.string,
+    Country: PropTypes.string,
+    Genre: PropTypes.string,
+    Language: PropTypes.string,
   }),
-  movieID: PropTypes.string
+  movieID: PropTypes.string,
 };
